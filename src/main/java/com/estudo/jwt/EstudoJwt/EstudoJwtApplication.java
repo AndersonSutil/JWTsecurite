1ac013369db439e56cb79b8bd3f614222086caca
AndersonSutil <adsslipknot@gmail.com>
2019-09-15 12:08:57 -0300

    COMMIT CONFIG PROJECT

---
+++ src/main/java/com/estudo/jwt/EstudoJwt/EstudoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.jwt.EstudoJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoJwtApplication.class, args);
+	}
+
+}
